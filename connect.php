68b9f5218b0a83cb3490489dc385f23d3d4bd118
Chanida K <fonsukoth@gmail.com>
2017-07-02 17:38:01 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
-<?
+<? //in my MAC; be careful cuz it doesn't has password
 $mysqli = new mysqli('localhost','root','','');
    if($mysqli->connect_errno){
       echo $mysqli->connect_errno.": ".$mysqli->connect_error;
